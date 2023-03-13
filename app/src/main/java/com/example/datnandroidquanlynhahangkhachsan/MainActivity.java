ed5f88d3bcf372c366c8a4e4e003613f43563804
lvvy0410 <levanvy.work.0337909492@gmail.com>
2023-03-13 17:10:12 +0700

    sửa phiếu nhận phòng, phiếu đặt phòng, thêm màn hình chi tiết phòng
    sửa phiếu nhận phòng, phiếu đặt phòng, thêm màn hình chi tiết phòng

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.datnandroidquanlynhahangkhachsan;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.datnandroidquanlynhahangkhachsan.adapter.MenuAdapter;
+import com.example.datnandroidquanlynhahangkhachsan.model.DichVu;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -24,12 +29,27 @@ import kotlinx.coroutines.Job;
 import kotlinx.coroutines.selects.SelectClause0;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rscvDichVu;
+    private List<DichVu> lsdichvu;
+    private MenuAdapter menuAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phieunhanphong);
-
-
+        setContentView(R.layout.activity_chitietphong);
+        rscvDichVu = findViewById(R.id.rscv_dichvu);
+        lsdichvu = new ArrayList<>();
+        DichVu dv1 = new DichVu( "dv1",  "1",  "1" , "1");
+        DichVu dv2 = new DichVu( "dv2",  "2", "2", "2");
+        DichVu dv3 = new DichVu( "dv3",  "3", "3", "3");
+        DichVu dv4 = new DichVu( "dv4",  "4", "4", "4");
+        lsdichvu.add(dv1);
+        lsdichvu.add(dv2);
+        lsdichvu.add(dv3);
+        lsdichvu.add(dv4);
+        menuAdapter = new MenuAdapter(lsdichvu);
+        LinearLayoutManager LinearLayoutManager = new LinearLayoutManager(this);
+        rscvDichVu.setLayoutManager(LinearLayoutManager);
+        rscvDichVu.setAdapter(menuAdapter);
     }
 }
---
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/adapter/MenuAdapter.java
@@ -0,0 +1,63 @@
+package com.example.datnandroidquanlynhahangkhachsan.adapter;
+
+import android.provider.DocumentsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.datnandroidquanlynhahangkhachsan.R;
+import com.example.datnandroidquanlynhahangkhachsan.model.DichVu;
+
+import java.util.List;
+
+public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder> {
+    private List<DichVu> lsdichvu;
+
+    public MenuAdapter(List<DichVu> lsdichvu) {
+        this.lsdichvu = lsdichvu;
+    }
+
+    @NonNull
+    @Override
+    public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_menu, parent, false);
+        return new MenuViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
+        DichVu dichvu = lsdichvu.get((position));
+        if (dichvu == null) {
+            return;
+        }
+        holder.tvtenmenu.setText(dichvu.getTenHangHoa());
+        holder.tvsoluongmenu.setText(dichvu.getSoLuong());
+        holder.tvgiamenu.setText(dichvu.getDonGia());
+        holder.tvPrice.setText(dichvu.getThanhTien());
+    }
+
+    @Override
+    public int getItemCount() {
+        if ((lsdichvu != null)) {
+            return lsdichvu.size();
+        }
+        return 0;
+    }
+
+    class MenuViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvtenmenu, tvgiamenu, tvsoluongmenu, tvPrice;
+
+        public MenuViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            tvtenmenu = itemView.findViewById(R.id.tv_tenmenu);
+            tvgiamenu = itemView.findViewById(R.id.tv_giamenu);
+            tvsoluongmenu = itemView.findViewById(R.id.tv_soluongmenu);
+            tvPrice = itemView.findViewById(R.id.tv_Price);
+        }
+    }
+}
