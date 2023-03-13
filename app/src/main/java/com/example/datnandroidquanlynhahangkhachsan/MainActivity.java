95cfc51c09229a31844a0fde8f2a0efec3df4b0f
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-03-13 17:10:57 +0700

    bottom_nav_ và một vài chỉnh sửa
    bottom_nav_ và một vài chỉnh sửa

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
@@ -1,35 +1,46 @@
 package com.example.datnandroidquanlynhahangkhachsan;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.gson.Gson;
+import com.example.datnandroidquanlynhahangkhachsan.databinding.ActivityMainBinding;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CancellationException;
-
-import kotlin.Unit;
-import kotlin.coroutines.Continuation;
-import kotlin.jvm.functions.Function1;
-import kotlin.sequences.Sequence;
-import kotlinx.coroutines.ChildHandle;
-import kotlinx.coroutines.ChildJob;
-import kotlinx.coroutines.DisposableHandle;
-import kotlinx.coroutines.Job;
-import kotlinx.coroutines.selects.SelectClause0;
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phieunhanphong);
-
-
+        binding =ActivityMainBinding .inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Fragment_dsPhong());
+     binding.bottomNavigationView.setOnItemSelectedListener(item->{
+         switch (item.getItemId()){
+             case  R.id.nav_danhsach:
+                 replaceFragment(new Fragment_dsPhong());
+                 break;
+             case R.id.nav_datphong:
+                 replaceFragment(new Fragment_dsPhieuDatPhong());
+                 break;
+             case R.id.nav_nhanphong:
+                 replaceFragment(new Fragment_dsPhieuNhanPhong());
+                 break;
+             case R.id.nav_lichsu:
+                 replaceFragment(new Fragment_dsLichSu());
+                 break;
+         }
+         return true;
+     });
     }
+private void replaceFragment(Fragment fragment)
+{
+    FragmentManager fragmentManager= getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.frame_layout,fragment);
+    fragmentTransaction.commit();
+}
 }
