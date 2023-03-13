6ae4102fd2e4d25288e6e25316bc9706aedd7b09
lvvy0410 <levanvy.work.0337909492@gmail.com>
2023-03-13 15:19:46 +0700

    abc
    abc

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phieudatphong);
+        setContentView(R.layout.activity_phieunhanphong);
 
 
     }
