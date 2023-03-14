4072b3d9f13d1ae7c1afa8846fb576e1fc5d281b
lvvy0410 <levanvy.work.0337909492@gmail.com>
2023-03-14 10:45:39 +0700

    phiếu trả phòng
    phiếu trả phòng

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
@@ -1,42 +1,23 @@
 package com.example.datnandroidquanlynhahangkhachsan;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.util.Log;
-
 import com.example.datnandroidquanlynhahangkhachsan.adapter.MenuAdapter;
-import com.example.datnandroidquanlynhahangkhachsan.databinding.ActivityMainBinding;
 import com.example.datnandroidquanlynhahangkhachsan.model.DichVu;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CancellationException;
-
-import kotlin.Unit;
-import kotlin.coroutines.Continuation;
-import kotlin.jvm.functions.Function1;
-import kotlin.sequences.Sequence;
-import kotlinx.coroutines.ChildHandle;
-import kotlinx.coroutines.ChildJob;
-import kotlinx.coroutines.DisposableHandle;
-import kotlinx.coroutines.Job;
-import kotlinx.coroutines.selects.SelectClause0;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rscvDichVu;
     private List<DichVu> lsdichvu;
     private MenuAdapter menuAdapter;
-    ActivityMainBinding binding;
+    //ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,36 +37,38 @@ public class MainActivity extends AppCompatActivity {
         menuAdapter = new MenuAdapter(lsdichvu);
         LinearLayoutManager LinearLayoutManager = new LinearLayoutManager(this);
         rscvDichVu.setLayoutManager(LinearLayoutManager);
+        RecyclerView.ItemDecoration decoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rscvDichVu.addItemDecoration(decoration);
         rscvDichVu.setAdapter(menuAdapter);
 
 // xử lý của bottom_nav
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        replaceFragment(new Fragment_dsPhong());
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.nav_danhsach:
-                    replaceFragment(new Fragment_dsPhong());
-                    break;
-                case R.id.nav_datphong:
-                    replaceFragment(new Fragment_dsPhieuDatPhong());
-                    break;
-                case R.id.nav_nhanphong:
-                    replaceFragment(new Fragment_dsPhieuNhanPhong());
-                    break;
-                case R.id.nav_lichsu:
-                    replaceFragment(new Fragment_dsLichSu());
-                    break;
-            }
-            return true;
-        });
-    }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        replaceFragment(new Fragment_dsPhong());
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()) {
+//                case R.id.nav_danhsach:
+//                    replaceFragment(new Fragment_dsPhong());
+//                    break;
+//                case R.id.nav_datphong:
+//                    replaceFragment(new Fragment_dsPhieuDatPhong());
+//                    break;
+//                case R.id.nav_nhanphong:
+//                    replaceFragment(new Fragment_dsPhieuNhanPhong());
+//                    break;
+//                case R.id.nav_lichsu:
+//                    replaceFragment(new Fragment_dsLichSu());
+//                    break;
+//            }
+//            return true;
+//        });
+//    }
+//
+//    private void replaceFragment(Fragment fragment) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.frame_layout, fragment);
+//        fragmentTransaction.commit();
     }
 }
 
