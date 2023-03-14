06c60e97e7c1aac2a141332292d5fd4fff68002b
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-03-14 08:35:27 +0700

    về ban đầu
    về ban đầu

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/MainActivity.java
@@ -36,19 +36,17 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rscvDichVu;
     private List<DichVu> lsdichvu;
     private MenuAdapter menuAdapter;
-    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chitietphong);
-        ///recyclerview
         rscvDichVu = findViewById(R.id.rscv_dichvu);
         lsdichvu = new ArrayList<>();
-        DichVu dv1 = new DichVu("dv1", "1", "1", "1");
-        DichVu dv2 = new DichVu("dv2", "2", "2", "2");
-        DichVu dv3 = new DichVu("dv3", "3", "3", "3");
-        DichVu dv4 = new DichVu("dv4", "4", "4", "4");
+        DichVu dv1 = new DichVu( "dv1",  "1",  "1" , "1");
+        DichVu dv2 = new DichVu( "dv2",  "2", "2", "2");
+        DichVu dv3 = new DichVu( "dv3",  "3", "3", "3");
+        DichVu dv4 = new DichVu( "dv4",  "4", "4", "4");
         lsdichvu.add(dv1);
         lsdichvu.add(dv2);
         lsdichvu.add(dv3);
@@ -57,36 +55,40 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager LinearLayoutManager = new LinearLayoutManager(this);
         rscvDichVu.setLayoutManager(LinearLayoutManager);
         rscvDichVu.setAdapter(menuAdapter);
-
-// xử lý của bottom_nav
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        replaceFragment(new Fragment_dsPhong());
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.nav_danhsach:
-                    replaceFragment(new Fragment_dsPhong());
-                    break;
-                case R.id.nav_datphong:
-                    replaceFragment(new Fragment_dsPhieuDatPhong());
-                    break;
-                case R.id.nav_nhanphong:
-                    replaceFragment(new Fragment_dsPhieuNhanPhong());
-                    break;
-                case R.id.nav_lichsu:
-                    replaceFragment(new Fragment_dsLichSu());
-                    break;
-            }
-            return true;
-        });
-    }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
     }
 }
-
-
+// xử lý của bottom_nav
+//public class MainActivity extends AppCompatActivity {
+//    ActivityMainBinding binding;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        binding =ActivityMainBinding .inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        replaceFragment(new Fragment_dsPhong());
+//        binding.bottomNavigationView.setOnItemSelectedListener(item->{
+//            switch (item.getItemId()){
+//                case  R.id.nav_danhsach:
+//                    replaceFragment(new Fragment_dsPhong());
+//                    break;
+//                case R.id.nav_datphong:
+//                    replaceFragment(new Fragment_dsPhieuDatPhong());
+//                    break;
+//                case R.id.nav_nhanphong:
+//                    replaceFragment(new Fragment_dsPhieuNhanPhong());
+//                    break;
+//                case R.id.nav_lichsu:
+//                    replaceFragment(new Fragment_dsLichSu());
+//                    break;
+//            }
+//            return true;
+//        });
+//    }
+//    private void replaceFragment(Fragment fragment)
+//    {
+//        FragmentManager fragmentManager= getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.frame_layout,fragment);
+//        fragmentTransaction.commit();
+//    }
+//}
