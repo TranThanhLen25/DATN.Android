607cf86e92ef37691919bc22e736eeba3d80aa48
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-04-04 08:34:03 +0700

    aaaaa
    aaaaa

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/model/api/IAPIServiceRetrofit.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/model/api/IAPIServiceRetrofit.java
@@ -13,25 +13,25 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-<<<<<<< HEAD
+
 import retrofit2.http.POST;
-=======
+
 import retrofit2.http.Query;
->>>>>>> develop
+
 
 public interface IAPIServiceRetrofit {
 
     @GET("/api/hanghoa/danhsach-hanghoa")
     Call<ResponseDTO<List<HangHoaDTO>>> layDanhSachHangHoa(@Body DieuKienLocHangHoaDTO dieuKienLoc);
 
-<<<<<<< HEAD
+
     @GET("/api/hanghoa/danhsach-hanghoa")
     Call<ResponseDTO<List<HangHoaDTO>>> layDanhSachHangHoa2();
 
     @POST("/api/Phong/danhsach-Phong")
     Call<ResponseDTO<List<PhongDTO>>> layDanhSachPhong();
-=======
+
     @GET("/api/hanghoa/danhsach-hanghoa?")
     Call<ResponseDTO<List<HangHoaDTO>>> layDanhSachHangHoa2(@Query("NhomHangHoa") String NhomHangHoa);
->>>>>>> develop
+
 }
