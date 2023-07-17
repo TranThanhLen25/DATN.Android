6cd92d5b993db40796b94013f4a6107314c017c2
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-07-17 11:14:21 +0700

    kiểm tra mã hóa
    kiểm tra mã hóa

--- app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/utils/PublicVariables.java
+++ app/src/main/java/com/example/datnandroidquanlynhahangkhachsan/utils/PublicVariables.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class PublicVariables {
 
-    public static String url_server= "http://192.168.1.64:8181";
+    public static String url_server= "http://172.16.1.46:8181";
 
     public static Date exp_access_token;
     public final static String error_msg_token = "Lấy access token thất bại.";
